53435cfc80e34bb6e499e9c5e529eef203829953
Motoi Taniguchi <sus_hl_h50@yahoo.co.jp>
2016-10-18 23:55:45 +0900

    初めてのコミットです。

---
+++ Assets/BlockSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockSound : MonoBehaviour {
+
+	public AudioClip sound;
+	private AudioSource  audioSource; 
+
+
+	void Start () {
+		audioSource	= gameObject.AddComponent< AudioSource >();
+	}
+
+	void OnCollisionEnter2D (Collision2D other){
+		if (other.gameObject.tag == "Block" || other.gameObject.tag == "Ground") {
+			audioSource.PlayOneShot(sound);
+			Debug.Log ("bang");
+
+			}
+	}
+}
